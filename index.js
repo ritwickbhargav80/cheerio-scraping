59b95a1c368ff0624e7347d4e820b9f845cf3137
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2019-07-02 15:27:31 +0530

    Contributions Done

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const cheerio = require('cheerio');
 
 request('https://github.com/RitwickBhargav', (error, response, html) => {
     if (!error && response.statusCode == 200) {
+	
         const $ = cheerio.load(html);
-
+	
+	console.log($('.js-yearly-contributions').find('h2').html());
         $('.mr-3').each((i, el) => {
             const item = $(el).find('span').text();
             console.log(item);
